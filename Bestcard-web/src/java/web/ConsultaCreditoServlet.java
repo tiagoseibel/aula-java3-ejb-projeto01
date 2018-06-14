89bc545713587d165c9b6e9d3a16155616721966
tiagoseibel <tiagoseibel@gmail.com>
2018-06-13 21:49:55 -0300

    Implementação de REST com Servlet

--- Bestcard-web/src/java/web/ConsultaCreditoServlet.java
+++ Bestcard-web/src/java/web/ConsultaCreditoServlet.java
@@ -1,7 +1,6 @@
 package web;
 
 import beans.ConsultaCreditoBeanRemote;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -9,8 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import static java.lang.Integer.parseInt;
-import java.math.BigDecimal;
-import java.util.stream.Collectors;
 import javax.ejb.EJB;
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -27,24 +24,22 @@ public class ConsultaCreditoServlet extends HttpServlet {
         PrintWriter saida = response.getWriter();
         
         int client_id = parseInt( request.getParameter("id") );
-//        saida.write("O credito do cliente é: " 
-//                + bean.getCredito(client_id) );
         
         JsonObject retorno = Json.createObjectBuilder()
-                .add("saldo", bean.getCredito(client_id) )
+                .add( "saldo", bean.getCredito(client_id) )
                 .build();
-       
-        String content = "";
-        try ( BufferedReader leitor = request.getReader() ) {
-            content = leitor.lines().collect( Collectors.joining() );
-        }
-        
+
         saida.write( retorno.toString() );
-        
     }
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+//                String content = "";
+//        try ( BufferedReader leitor = request.getReader() ) {
+//            content = leitor.lines().collect( Collectors.joining() );
+//        }
+        
     }
 }
--- Bestcard-web/src/java/web/RegistraCompraServlet.java
+++ Bestcard-web/src/java/web/RegistraCompraServlet.java
@@ -1,39 +1,50 @@
 package web;
 
 import beans.ConsultaCreditoBeanRemote;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import static java.lang.Integer.parseInt;
-import static java.lang.Double.parseDouble;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.stream.Collectors;
 import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
-public class RegistraCompraServlet extends HttpServlet{
+public class RegistraCompraServlet extends HttpServlet {
 
     @EJB
     private ConsultaCreditoBeanRemote bean;
-    
+
     @Override
-    protected void doPost(HttpServletRequest req, 
-            HttpServletResponse resp) 
-            throws ServletException, IOException
-    {
-        resp.setContentType("text/html");
+    protected void doPost(HttpServletRequest req,
+            HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("application/json");
         PrintWriter saida = resp.getWriter();
-        
-        int id_compra = parseInt( req.getParameter("id_compra") );
-        int id_cliente = parseInt( req.getParameter("id_cliente") );
-        String data_compra = req.getParameter("data_compra");
-        String nome_loja = req.getParameter("nome_loja");
-        double valor_compra = 
-                parseDouble( req.getParameter("valor_compra") );
-        
+
+        String content = "";
+        try (BufferedReader leitor = req.getReader()) {
+            content = leitor.lines().collect(Collectors.joining());
+        }
+
+        // JsonParse (String -> JSON object
+        JsonReader reader = Json.createReader(new StringReader(content));
+        JsonObject compra = reader.readObject();
+
+        int id_compra = compra.getJsonNumber("id").intValue();
+        int id_cliente = compra.getJsonNumber("id_cliente").intValue();
+        String nome_loja = compra.getJsonString("nomeLoja").getString();
+        String data_compra = compra.getJsonString("dataCompra").getString();
+        double valor_compra = compra.getJsonNumber("valorCompra").doubleValue();
+
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         Date data = null;
         try {
@@ -41,16 +52,17 @@ public class RegistraCompraServlet extends HttpServlet{
         } catch (ParseException ex) {
             throw new ServletException(ex);
         }
-        
-        boolean ret = bean.registrarCompra (
-                        id_cliente, nome_loja, 
-                        data, valor_compra, id_compra
-                      );
-        
+
+        boolean ret = bean.registrarCompra(
+                id_cliente, nome_loja,
+                data, valor_compra, id_compra
+        );
+
         if (ret) {
-            saida.write("OK");
-        } else { 
-            saida.write("Saldo insuficiente para compra!"); 
+            saida.write("{ \"msg\": \"OK\" }");
+        } else {
+            saida.write("{ \"msg\": \"Saldo insuficiente!\" }");
         }
+
     }
 }
